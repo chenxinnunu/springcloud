57265b9831b5e26be77c27d74fb07d8908aadab0
group~1 <chenxin@chensanjin.xin>
2019-01-17 18:12:55 +0800

    配置spring cloud config 服务端

---
+++ config-server/src/main/java/com/ruicheng/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.ruicheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+//激活对配置中心的支持
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
