f1cef473f9e39b01e9c41681a00f3ab5975053ec
group~1 <chenxin@chensanjin.xin>
2019-01-18 16:19:03 +0800

    增加trubine由消息中间价收集监控数据

--- hystrix-turbine/src/main/java/com/ruicheng/HystrixTurbineApplication.java
+++ hystrix-turbine/src/main/java/com/ruicheng/HystrixTurbineApplication.java
@@ -3,7 +3,7 @@ package com.ruicheng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 /**
  * Turbin提供把多个hystrix.stream的内容聚合为一个数据源供Dashboard展示
@@ -12,8 +12,9 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
  * @date 2019/1/16 21:38
  */
 @SpringBootApplication
-//激活对Turbine的支持
-@EnableTurbine
+//激活对Turbine的支持,使用了消息中间件收集监控数据，此注解不在适用
+//@EnableTurbine
+@EnableTurbineStream
 @EnableDiscoveryClient
 public class HystrixTurbineApplication {
     public static void main(String[] args) {
