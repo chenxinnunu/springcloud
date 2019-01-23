4a4ff0d447d667b6d60fd2cfb98db899f12dc23e
group~1 <chenxin@chensanjin.xin>
2019-01-23 14:02:58 +0800

    增加Dockerfile 和 docker-compose

--- config-server/src/main/java/com/ruicheng/ConfigServerApplication.java
+++ config-server/src/main/java/com/ruicheng/ConfigServerApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableDiscoveryClient
 //激活对配置中心的支持
 @EnableConfigServer
 @SpringBootApplication
+@RefreshScope
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
